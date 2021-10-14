fa27de1c46bbe575b2a88cd026b5ec3ce9e3a234
vaishnavi0701 <90175670+vaishnavi0701@users.noreply.github.com>
2021-10-14 12:12:04 +0530

    Add files via upload

---
+++ words.py
@@ -0,0 +1,22 @@
+import string
+import random
+
+def load_words():
+    """
+    Ye function kaafi jayada words ko load karne mai help karega
+    """
+    with open("/home/vinaata/Desktop/vaishnavi_hangman/hangman/words.txt","r")as word_file:
+        data=word_file.read()
+        word_list=data.split()
+    return word_list
+
+def choose_word():
+    """
+    word_list (list): list of words (strings)
+    ye function ek word randomly return karega
+    """
+    word_list = load_words()
+    secret_word = random.choice(word_list)
+    secret_word = secret_word.lower()
+
+    return secret_word
